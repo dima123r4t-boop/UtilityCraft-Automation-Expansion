e3db80f430518647dc6d3f9e9b1e2da1bc973f10
dima123r4t-boop <dima123r4t@gmail.com>
2026-07-29 15:29:18 -0300

    Update main.js

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -1,12 +1,23 @@
 import { system } from "@minecraft/server";
 import { Logger } from "./core/logger.js";
 import { Automation } from "./core/automation.js";
+import { RecipeManager } from "./core/recipeManager.js";
 
 system.run(() => {
 
     Logger.info("--------------------------------");
-    Logger.info("UtilityCraft Automation Loaded");
-    Logger.info("Version: " + Automation.VERSION);
+    Logger.info("UtilityCraft Automation");
+    Logger.info("Version " + Automation.VERSION);
     Logger.info("--------------------------------");
+
+    RecipeManager.register("minecraft:planks", {
+        output: 4,
+        ingredients: [
+            {
+                item: "minecraft:log",
+                count: 1
+            }
+        ]
+    });
 
 });
