651e0f71ad0752adb9a4dc718fc2457f31cfdaf1
dima123r4t-boop <dima123r4t@gmail.com>
2026-07-29 15:43:29 -0300

    Update autoCrafter.js

--- BP/scripts/machines/autoCrafter.js
+++ BP/scripts/machines/autoCrafter.js
@@ -1,25 +1,41 @@
 import { Logger } from "../core/logger.js";
 import { CraftingJob } from "../core/craftingJob.js";
+import { CraftingPlanner } from "../core/craftingPlanner.js";
 
 export class AutoCrafter {
 
     constructor(location) {
+
         this.location = location;
         this.jobs = [];
+
     }
 
     addJob(itemId, amount) {
+
+        if (!CraftingPlanner.canCraft(itemId)) {
+
+            Logger.info("Materiais insuficientes.");
+
+            return false;
+
+        }
+
         this.jobs.push(new CraftingJob(itemId, amount));
-        Logger.info("Novo trabalho: " + itemId);
+
+        Logger.info("Novo trabalho criado.");
+
+        return true;
+
     }
 
     update() {
 
-        for (const job of this.jobs) {
+        for (const job of this.jobs)
             job.tick();
-        }
 
         this.jobs = this.jobs.filter(job => !job.finished);
+
     }
 
 }
